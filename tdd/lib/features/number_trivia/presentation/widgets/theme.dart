dd207d5ff713f36cbe5e1d7a3d1be52bc79efcfc
Prabesh <prabesh.bista369@gmail.com>
2022-01-10 15:35:41 +0545

    Ui

--- tdd/lib/features/number_trivia/presentation/widgets/theme.dart
+++ tdd/lib/features/number_trivia/presentation/widgets/theme.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Mytheme {
   static lightTheme(BuildContext context) => ThemeData(
+    
     primarySwatch:Colors.lightGreen,
     appBarTheme: AppBarTheme(
+      
+      systemOverlayStyle:const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness:Brightness.light,
+      ),
       elevation: 0.0,
       backgroundColor: Colors.green[300],
      titleTextStyle: const TextStyle(
