2f75d108b7cf386de3198b61767b88db0f2e94e1
Prabesh <prabesh.bista369@gmail.com>
2022-01-10 14:59:38 +0545

    learning TDD

--- tdd/lib/main.dart
+++ tdd/lib/main.dart
@@ -1,45 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tdd/features/number_trivia/presentation/pages/app_view.dart';
+import 'package:tdd/features/number_trivia/presentation/widgets/theme.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool selected = false;
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedContainer(
-              width: selected?400.0:200.0,
-              height:selected?300.0:200.0 ,
-              duration: const Duration(seconds: 1),
-              curve: Curves.fastOutSlowIn,
-              child: Image.network(
-                  "https://pbs.twimg.com/media/Eu7kZRRWgAMJjj8.png"),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                        selected = !selected;
-                  });
-              
-                },
-                child: const Text("Click me"))
-          ],
-        ),
-      ),
+    return  MaterialApp(
+      home:const  HomePage(),
+      themeMode: ThemeMode.light,
+      theme: Mytheme.lightTheme(context),
     );
   }
 }
