67e03ff62557422bf8cfd257d06e8489dec65400
Prabesh <prabesh.bista369@gmail.com>
2022-01-10 14:59:41 +0545

    learning TDD

--- tdd/lib/features/number_trivia/presentation/widgets/theme.dart
+++ tdd/lib/features/number_trivia/presentation/widgets/theme.dart
@@ -1,8 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Mytheme {
   static lightTheme(BuildContext context) => ThemeData(
-    primarySwatch:Colors.lightGreen
+    primarySwatch:Colors.lightGreen,
+    appBarTheme: AppBarTheme(
+      elevation: 0.0,
+      backgroundColor: Colors.green[300],
+     titleTextStyle: const TextStyle(
+       color: Colors.white
+     )
+    )
   );
 }
