98a02d227dd2e0c054694552409ad6b296de7cf3
Prabesh <prabesh.bista369@gmail.com>
2022-01-10 15:43:27 +0545

    UI for Number Trivia

--- tdd/lib/features/number_trivia/presentation/pages/app_view.dart
+++ tdd/lib/features/number_trivia/presentation/pages/app_view.dart
@@ -9,6 +9,31 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title:const Text("Number trivia"),
       ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0,right: 16.0),
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              decoration:const  InputDecoration(
+                hintText: "Enter a Number",
+                border: OutlineInputBorder(
+                  borderRadius:BorderRadius.all(Radius.circular(12.0))
+                )
+              ),
+            ),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(onPressed: (){}, child:const  Text("Number Trivia")),
+              ElevatedButton(onPressed: (){}, child: const Text("Random Trivia"))
+            ],
+          )
+        ],
+      ),
     );
   }
 }
