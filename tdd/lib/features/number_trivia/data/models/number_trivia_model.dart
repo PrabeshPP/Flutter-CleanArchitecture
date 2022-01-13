c5c7afdce83de77a8455849c8f340651cca536bf
Prabesh <prabesh.bista369@gmail.com>
2022-01-13 17:37:57 +0545

    test passed for toJson and FromJSon()

--- tdd/lib/features/number_trivia/data/models/number_trivia_model.dart
+++ tdd/lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -6,5 +6,7 @@ class NumberTriviaModel extends NumberTrivia {
   NumberTriviaModel({required String text, required int number})
       : super(text: text, number: number);
   factory NumberTriviaModel.fromJson(Map<String, dynamic> json) =>
-      NumberTriviaModel(text: json['text'], number: json['number']);
+      NumberTriviaModel(
+          text: json['text'], number: (json['number'] as num).toInt());
+  Map<String, dynamic> toJSon() => {"text": text, "number": number};
 }
--- tdd/test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ tdd/test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:tdd/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,9 +6,9 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
-  final tNumberTrivia = NumberTriviaModel(text: "Test Text", number: 1);
+  final tNumberTriviaModel = NumberTriviaModel(text: "Test Text", number: 1);
   test("Should be a subclass of the NumberTriviaEntity", () async {
-    expect(tNumberTrivia, isA<NumberTrivia>());
+    expect(tNumberTriviaModel, isA<NumberTrivia>());
   });
 
   group("fromJson", () {
@@ -18,7 +17,25 @@ void main() {
       final Map<String, dynamic> jsonMap =
           await jsonDecode(reader("trivia.json"));
       final result = NumberTriviaModel.fromJson(jsonMap);
-      expect(result, equals(tNumberTrivia));
+      expect(result, equals(tNumberTriviaModel));
+    });
+    test("Should return a valid model when the Json Number is double",
+        () async {
+      final Map<String, dynamic> jsonMap1 =
+          await jsonDecode(reader("trivia_double.json"));
+      final result = NumberTriviaModel.fromJson(jsonMap1);
+      expect(result, tNumberTriviaModel);
+    });
+  });
+
+  group('toJson', () {
+    test("Should return the proper data for our model", () async {
+      final result = tNumberTriviaModel.toJSon();
+      final expectedMap = {
+        "text": "Test Text",
+        "number": 1,
+      };
+      expect(result, expectedMap);
     });
   });
 }
