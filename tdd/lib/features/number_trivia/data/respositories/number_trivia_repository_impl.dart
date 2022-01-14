53d763d3e72fdb257076adb370d8d83df2cc956c
Prabesh <prabesh.bista369@gmail.com>
2022-01-14 16:59:25 +0545

    repository(),cache

--- tdd/lib/features/number_trivia/data/respositories/number_trivia_repository_impl.dart
+++ tdd/lib/features/number_trivia/data/respositories/number_trivia_repository_impl.dart
@@ -1,18 +1,24 @@
+import 'package:tdd/core/platform/network_info.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_remote_sources.dart';
 import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
-class NumberTriviaRepositoryImpl implements NumberTriviaRepository{
-  @override
-  Future<Either<Failure, NumberTrivia>>? getConcreteNumberTrivia(int? number) {
-    
-    
-  }
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaLocalDataSource numberTriviaLocalDataSource;
+  final NumberTriviaRemoteSources numberTriviaRemoteSources;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {required this.numberTriviaLocalDataSource,
+      required this.numberTriviaRemoteSources,
+      required this.networkInfo});
 
   @override
-  Future<Either<Failure, NumberTrivia>>? getRandomNumberTrivia() {
-    
-  }
+  Future<Either<Failure, NumberTrivia>>? getConcreteNumberTrivia(int? number) {}
 
+  @override
+  Future<Either<Failure, NumberTrivia>>? getRandomNumberTrivia() {}
 }
--- tdd/test/features/number_trivia/data/repositories/number_trivia_repositories_impl_test.dart
+++ tdd/test/features/number_trivia/data/repositories/number_trivia_repositories_impl_test.dart
@@ -1,3 +1,32 @@
-void main(){
-  
+import 'package:mockito/mockito.dart';
+import 'package:tdd/core/platform/network_info.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_remote_sources.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd/features/number_trivia/data/respositories/number_trivia_repository_impl.dart';
+
+class Mocktriviaremotedatasource extends Mock
+    implements NumberTriviaRemoteSources {}
+
+class MockLocalDataSource extends Mock implements NumberTriviaLocalDataSource {}
+
+class MockNetworkInfo extends Mock implements NetworkInfo {}
+
+void main() {
+  NumberTriviaRepositoryImpl repositoryImpl;
+  Mocktriviaremotedatasource mocktriviaremotedatasource;
+  MockLocalDataSource mockLocalDataSource;
+  MockNetworkInfo mockNetworkInfo;
+
+  setUp(() {
+    mocktriviaremotedatasource = Mocktriviaremotedatasource();
+    mockLocalDataSource = MockLocalDataSource();
+    mockNetworkInfo = MockNetworkInfo();
+    repositoryImpl = NumberTriviaRepositoryImpl(
+        numberTriviaRemoteSources: mocktriviaremotedatasource,
+        numberTriviaLocalDataSource: mockLocalDataSource,
+        networkInfo: mockNetworkInfo);
+  });
+
+  test("Should  ", () {});
 }
