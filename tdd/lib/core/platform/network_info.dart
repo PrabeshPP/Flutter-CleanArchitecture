5a3b509af984be4d4912a131890b5961dc7ddcaa
Prabesh <prabesh.bista369@gmail.com>
2022-01-19 14:46:52 +0545

    connectivity_plus

--- tdd/lib/core/platform/network_info.dart
+++ tdd/lib/core/platform/network_info.dart
@@ -1,6 +1,13 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+
 abstract class NetworkInfo {
   Future<bool>? get isConnected;
 }
 
+class NetworkInfoImpl implements NetworkInfo {
+  final Connectivity? connectivity;
 
-
+  NetworkInfoImpl({required this.connectivity});
+  @override
+  Future<bool>? get isConnected => throw UnimplementedError();
+}
