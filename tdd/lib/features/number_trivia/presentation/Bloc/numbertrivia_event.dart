c0ca03e76b3876ad77f2fdd6e2b659d214a5c800
Prabesh <prabesh.bista369@gmail.com>
2022-01-24 15:05:18 +0545

    bloc

---
+++ tdd/lib/features/number_trivia/presentation/Bloc/numbertrivia_event.dart
@@ -0,0 +1,8 @@
+part of 'numbertrivia_bloc.dart';
+
+abstract class NumbertriviaEvent extends Equatable {
+  const NumbertriviaEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ tdd/lib/features/number_trivia/presentation/Bloc/numbertrivia_state.dart
@@ -0,0 +1,10 @@
+part of 'numbertrivia_bloc.dart';
+
+abstract class NumbertriviaState extends Equatable {
+  const NumbertriviaState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+class NumbertriviaInitial extends NumbertriviaState {}
