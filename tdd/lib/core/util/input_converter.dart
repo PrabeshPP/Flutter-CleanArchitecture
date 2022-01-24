d8e18f49edf0dc8af8adf2b0a414f97c4c525f5c
Prabesh <prabesh.bista369@gmail.com>
2022-01-24 16:28:31 +0545

    test passed for the number input converte class

--- tdd/lib/core/util/input_converter.dart
+++ tdd/lib/core/util/input_converter.dart
@@ -3,10 +3,13 @@ import 'package:tdd/core/error/failure.dart';
 
 class InputConverter {
   Either<Failure, int>? stringToInt(String number1) {
-    
+    try {
+      int number = int.parse(number1);
+      return Right(number);
+    } on Exception {
+      return Left(InvalidInputFailure());
+    }
   }
 }
 
-class InvalidInputFailure extends Failure{
-
-}
+class InvalidInputFailure extends Failure {}
--- tdd/test/core/util/input_converter_test.dart
+++ tdd/test/core/util/input_converter_test.dart
@@ -1,6 +1,35 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:tdd/core/util/input_converter.dart';
 
-class MockInputConverter extends Mock implements InputConverter {}
+void main() {
+  InputConverter? inputConverter;
+  setUp(() {
+    inputConverter = InputConverter();
+  });
 
+  group("StringToUnsignedInteger", () {
+    test("Should return an integer when the string is Unsigned Integer",
+        () async {
+      // arrange
+      String tNUmberString = "10";
+      // act
+      final result = inputConverter!.stringToInt(tNUmberString);
+      // assert
+      expect(result, const Right(10));
+    });
+
+    test("Should throw an error when the String is not an Unsigned Integer",
+        () async {
+      //arrange
+      String tNumberString = "Prabesh";
+
+      //act
+      final result = inputConverter!.stringToInt(tNumberString);
+
+      //assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
