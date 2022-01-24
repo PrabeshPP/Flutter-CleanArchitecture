76272eccac539cd6be3d0de9fcab0cc801b9ff78
Prabesh <prabesh.bista369@gmail.com>
2022-01-24 16:15:05 +0545

    implementing the number converter and test method

--- tdd/lib/core/util/input_converter.dart
+++ tdd/lib/core/util/input_converter.dart
@@ -2,8 +2,11 @@ import 'package:dartz/dartz.dart';
 import 'package:tdd/core/error/failure.dart';
 
 class InputConverter {
-  Either<Failure, int> StringtoInt(String number1) {
-    int number = int.parse(number1);
-    return Right(number);
+  Either<Failure, int>? stringToInt(String number1) {
+    
   }
+}
+
+class InvalidInputFailure extends Failure{
+
 }
--- tdd/test/core/util/input_converter_test.dart
+++ tdd/test/core/util/input_converter_test.dart
@@ -4,20 +4,3 @@ import 'package:tdd/core/util/input_converter.dart';
 
 class MockInputConverter extends Mock implements InputConverter {}
 
-void main() {
-  MockInputConverter? mockInputConverter;
-
-  setUp(() {
-    mockInputConverter = MockInputConverter();
-  });
-  String tNumberString = "5";
-
-  test("Should convert the given String number to integer ", () {
-    // arrange
-   
-    // act
-   
-    // verify
-  
-  });
-}
