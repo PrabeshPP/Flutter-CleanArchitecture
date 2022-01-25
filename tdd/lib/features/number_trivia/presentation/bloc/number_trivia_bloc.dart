885b85f9309e88e6ad685fa6e2613fe0c80c5a11
Prabesh <prabesh.bista369@gmail.com>
2022-01-25 13:21:44 +0545

    testing the Number Trivia Bloc

--- tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ tdd/lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,13 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:tdd/core/util/input_converter.dart';
+import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(NumberTriviaInitial()) {
-    on<NumberTriviaEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final GetConcreteNumberTrivia _getConcreteNumberTrivia;
+  final GetRandomNumberTrivia _getRandomNumberTrivia;
+  final InputConverter _inputConverter;
+  NumberTriviaBloc(
+      {required GetConcreteNumberTrivia getConcreteNumberTrivia,
+      required GetRandomNumberTrivia getRandomNumberTrivia,
+      required InputConverter inputConverter})
+      : _getConcreteNumberTrivia = getConcreteNumberTrivia,
+        _getRandomNumberTrivia = getRandomNumberTrivia,
+        _inputConverter=inputConverter,
+        super(Empty()) {
+    on<NumberTriviaEvent>((event, emit) {});
+    on<GetTriviaForConcreteNumber>(_onGetTriviaConcreteNumber);
   }
+
+  void _onGetTriviaConcreteNumber(
+      NumberTriviaEvent event, Emitter<NumberTriviaState> emit) async {}
 }
--- tdd/lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
+++ tdd/lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -2,9 +2,28 @@ part of 'number_trivia_bloc.dart';
 
 abstract class NumberTriviaState extends Equatable {
   const NumberTriviaState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-class NumberTriviaInitial extends NumberTriviaState {}
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia numberTrivia;
+  const Loaded({required this.numberTrivia});
+
+  @override
+  List<Object> get props => [numberTrivia];
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  const Error(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
