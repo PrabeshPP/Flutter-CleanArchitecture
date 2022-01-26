23a0492050f0b52f74c33c43d4a0bf344d93e56d
Prabesh <prabesh.bista369@gmail.com>
2022-01-26 14:33:57 +0545

    injected dependecy

--- tdd/lib/injection_container.dart
+++ tdd/lib/injection_container.dart
@@ -1,17 +1,64 @@
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tdd/core/platform/network_info.dart';
+import 'package:tdd/core/util/input_converter.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:tdd/features/number_trivia/data/datasources/number_trivia_remote_sources.dart';
+import 'package:tdd/features/number_trivia/data/respositories/number_trivia_repository_impl.dart';
+import 'package:tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:tdd/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:tdd/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 import 'package:tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:http/http.dart' as http;
 
 final getit = GetIt.instance;
 
-void init() {
+Future<void> init() async{
   //! Features -NumberTrivia
   //bloc
-  getit.registerFactory(() => NumberTriviaBloc(getConcreteNumberTrivia: getit(),
-   getRandomNumberTrivia: getit()
-   , inputConverter: getit()
-   )
-   );
+  getit.registerFactory(() => NumberTriviaBloc(
+      getConcreteNumberTrivia: getit(),
+      getRandomNumberTrivia: getit(),
+      inputConverter: getit()));
+  //Use Cases
+  getit.registerLazySingleton(() => GetConcreteNumberTrivia(getit()));
+  getit.registerLazySingleton(() => GetRandomNumberTrivia(repository: getit()));
+
+  //Repository
+  getit.registerLazySingleton<NumberTriviaRepository>(() =>
+      NumberTriviaRepositoryImpl(
+          localDataSource: getit(),
+          remoteSources: getit(),
+          networkInfo: getit()));
+
+  //Data Sources
+
+  //Local Data Source
+  getit.registerLazySingleton<NumberTriviaLocalDataSource>(
+      () => NumberTriviaLocalDataSourceImpl(sharedPreferences: getit()));
+  
+
+  //Remote Data Source
+
+  getit.registerLazySingleton<NumberTriviaRemoteSources>(
+      () => NumberTriviaRemoteSourcesImpl(httpClient: getit()));
+  
+
+  
+
+
   //! Core
+  getit.registerLazySingleton(() => InputConverter());
+  //NetworkInfo
+  getit.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(internetConnectionChecker: getit()));
+  
 
   //! External
+  final sharedPreferences=await SharedPreferences.getInstance();
+  getit.registerLazySingleton(() =>sharedPreferences );
+  getit.registerLazySingleton(() => http.Client);
+  getit.registerLazySingleton(() => InternetConnectionChecker());
+
 }
--- tdd/lib/main.dart
+++ tdd/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tdd/features/number_trivia/presentation/pages/app_view.dart';
 import 'package:tdd/features/number_trivia/presentation/widgets/theme.dart';
+import 'package:tdd/injection_container.dart' as di;
 
-
-void main() {
+void main() async{
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const  HomePage(),
+      home: const HomePage(),
       themeMode: ThemeMode.light,
       theme: Mytheme.lightTheme(context),
     );
