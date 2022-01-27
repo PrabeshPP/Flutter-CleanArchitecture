91b495db306ec5d9bb4fbc7006ecf738a8b34256
Prabesh <prabesh.bista369@gmail.com>
2022-01-27 16:13:37 +0545

    widgets

--- tdd/lib/features/number_trivia/presentation/pages/Number_trivia_page.dart
+++ tdd/lib/features/number_trivia/presentation/pages/Number_trivia_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:tdd/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
 import 'package:tdd/injection_container.dart';
 
@@ -26,10 +27,18 @@ class HomePage extends StatelessWidget {
                 padding: const EdgeInsets.all(12.0),
                 child: BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
                     builder: (context, state) {
-                  if (state is Empty) {}
-                  return Container(
-                      height: MediaQuery.of(context).size.height / 3,
-                      child: const Text("Start Searching"));
+                 
+                  if (state is Empty) {
+                    return const DispalyMessage(message: "Start Searching!");
+                  } else if (state is Loading) {
+                    return const LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDispaly(numberTrivia: state.numberTrivia);
+                  } else if (state is Error) {
+                    return DispalyMessage(message: state.message);
+                  } else {
+                    return const Text("Hello");
+                  }
                 })),
             Padding(
               padding: const EdgeInsets.only(left: 16.0, right: 16.0),
@@ -55,5 +64,71 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class DispalyMessage extends StatelessWidget {
+  final String message;
+  const DispalyMessage({Key? key, required this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height / 3,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Text(
+              message,
+              style: const TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ));
+    ;
+  }
+}
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height / 3,
+        child: const Center(child: CircularProgressIndicator()));
+  }
+}
+
+class TriviaDispaly extends StatelessWidget {
+  final NumberTrivia numberTrivia;
+  const TriviaDispaly({Key? key, required this.numberTrivia}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height / 3,
+        child:  Column(
+            children: [
+              Text(
+                numberTrivia.number.toString(),
+                style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Text(
+                      numberTrivia.text,
+                      style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+    ;
   }
 }
